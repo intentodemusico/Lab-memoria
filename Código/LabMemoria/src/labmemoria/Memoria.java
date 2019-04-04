1c6a0ddfc4e7696b57170393cff06b476850bf60
intentodemusico <44100370+intentodemusico@users.noreply.github.com>
2019-04-04 08:57:30 -0500

    Update Memoria.java
    Creación de memoria

--- Código/LabMemoria/src/labmemoria/Memoria.java
+++ Código/LabMemoria/src/labmemoria/Memoria.java
@@ -5,10 +5,25 @@
  */
 package labmemoria;
 
+import java.util.Random;
+
 /**
  *
  * @author INTENTODEMUSICO
  */
 public class Memoria {
+    int cantidadDeEspacios=64;
+
+    public Memoria() {
+        
+    }
+    
+    private void crearArray(){
+    Random rd = new Random(); // creating Random object
+      int [] memoria = new int[cantidadDeEspacios];
+      for (int i = 0; i < memoria.length; i++) {
+         memoria[i] = rd.nextInt(); // storing random integers in an array
+        }
+    }
     
 }
