ae8f3051261ae6c0eea5d2cdaff68fcae35769df
? <alberto.garcia.2016@upb.edu.co>
2019-04-11 09:04:29 -0500

    Using test file as default

--- Código/LabMemoria/src/labmemoria/Espacio.java
+++ Código/LabMemoria/src/labmemoria/Espacio.java
@@ -4,11 +4,48 @@
  * and open the template in the editor.
  */
 package labmemoria;
-
+import java.util.Random;
 /**
  *
  * @author INTENTODEMUSICO
  */
 public class Espacio {
+    private Random rd = new Random();
+    private boolean asignado=false;
+    private int id=0;
+    private int tamaño;
+    public Espacio() {
+        if(quedaLleno()){
+            generarId();
+        }
+    }
+    
+    private boolean quedaLleno(){
+        boolean bool=rd.nextBoolean();
+        return bool;
+    }
+   
+    private void generarId(){
+     // creating Random object
+     while(id==0){
+        id = rd.nextInt(); // storing random integers in an array
+     }
+    }
+    
+    private void generarTamaño(){
+    tamaño=rd.nextInt(7)+2;
+    }
+
+    public boolean isAsignado() {
+        return asignado;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getTamaño() {
+        return tamaño;
+    }
     
 }
--- Código/LabMemoria/src/labmemoria/LabMemoria.java
+++ Código/LabMemoria/src/labmemoria/LabMemoria.java
@@ -16,6 +16,7 @@ public class LabMemoria {
      */
     public static void main(String[] args) {
         // Soy Fabs
+        
     }
     
 }
