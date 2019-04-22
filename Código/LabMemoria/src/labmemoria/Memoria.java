bf81443bea3597b8360f45aecc01e28a1bce90b4
PapiCarryFabs <fabiandresbarreto@gmail.com>
2019-04-21 23:06:15 -0500

    Update Memoria.java

--- Código/LabMemoria/src/labmemoria/Memoria.java
+++ Código/LabMemoria/src/labmemoria/Memoria.java
@@ -4,57 +4,57 @@
  * and open the template in the editor.
  */
 package labmemoria;
+
 import java.util.Arrays;
 import java.util.LinkedList;
+
 /**
  *
  * @author INTENTODEMUSICO
  */
-public class Memoria extends LinkedList<Espacio>{
+public class Memoria extends LinkedList<Espacio> {
 
-    
-    int ocupados, espacioDisponible,iteracionLlenado=0;
-    private int [] vectorMemoria;
-    public Memoria(){
+    int ocupados, espacioDisponible, iteracionLlenado = 0;
+    private int[] vectorMemoria;
+
+    public Memoria() {
         reiniciarMemoria();
-    }   
-    
-    private void llenarMemoria(){
-        while (espacioDisponible!=0) {            
-           Espacio bloque =new Espacio (espacioDisponible);
-           for (int i=bloque.getInicio();i<bloque.getInicio()+bloque.getTamaño();i++){
-               vectorMemoria[i]=bloque.getId(); //hasta vector[ocupados+bloque.getTamaño()-1]
-           }
-           espacioDisponible-=bloque.getTamaño(); //Resta tamaño de espacioDisponible
-           ocupados++; //Hay más espacios ocupados
-           //Agregar a la lista enlazada
-           this.add(bloque);
+    }
+
+    private void llenarMemoria() {
+        while (espacioDisponible != 0) {
+            Espacio bloque = new Espacio(espacioDisponible);
+            for (int i = bloque.getInicio(); i < bloque.getInicio() + bloque.getTamaño(); i++) {
+                vectorMemoria[i] = bloque.getId(); //hasta vector[ocupados+bloque.getTamaño()-1]
+            }
+            espacioDisponible -= bloque.getTamaño(); //Resta tamaño de espacioDisponible
+            ocupados++; //Hay más espacios ocupados
+            //Agregar a la lista enlazada
+            this.add(bloque);
         }
-        if(ocupados<4 || ocupados>32){
+        if (ocupados < 4 || ocupados > 32) {
             iteracionLlenado++;
             reiniciarMemoria();
-        }else{
-            System.out.println("Memoria llenada con éxito en la "+iteracionLlenado+" iteración");
+        } else {
+            System.out.println("Memoria llenada con éxito en la " + iteracionLlenado + " iteración");
         }
     }
-    
-    private void reiniciarMemoria(){
+
+    private void reiniciarMemoria() {
         reiniciarValores();
         llenarMemoria();
     }
-     
-    private void reiniciarValores(){
-        vectorMemoria=new int[64] ;
-        ocupados=0;
-        espacioDisponible=64;
+
+    private void reiniciarValores() {
+        vectorMemoria = new int[64];
+        ocupados = 0;
+        espacioDisponible = 64;
     }
-    
-    public String toString(){return Arrays.toString(vectorMemoria);}
-    
-    public String toString(){
+
+    public String toString() {
         return Arrays.toString(vectorMemoria);
     }
-    
+
     @Override
     public boolean add(Espacio e) {
         return super.add(e); //To change body of generated methods, choose Tools | Templates.
